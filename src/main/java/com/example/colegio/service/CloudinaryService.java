249d8ca9939badcb0695e375372328b6bcb73267
carranzapacheco <carranzapacheco.f.a@gmail.com>
2025-12-12 15:58:47 -0500

    Agregar CloudinaryService.java

---
+++ src/main/java/com/example/colegio/service/CloudinaryService.java
@@ -0,0 +1,29 @@
+package com.example.colegio.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public String upload(MultipartFile file, String nombrePublico) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap(
+                        "public_id", nombrePublico,
+                        "folder", "colegio"   // carpeta opcional en Cloudinary
+                )
+        );
+
+        return uploadResult.get("secure_url").toString();
+    }
+}
