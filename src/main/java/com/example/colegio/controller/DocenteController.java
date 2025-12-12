6adc8eb79e04085303d0911d7d0d4c3f524f1a7b
carranzapacheco <carranzapacheco.f.a@gmail.com>
2025-12-12 11:44:01 -0500

    Actualizando docenteController

--- src/main/java/com/example/colegio/controller/DocenteController.java
+++ src/main/java/com/example/colegio/controller/DocenteController.java
@@ -6,22 +6,17 @@ import com.example.colegio.model.Usuario;
 import com.example.colegio.service.DocenteService;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class DocenteController {
@@ -35,6 +30,7 @@ public class DocenteController {
     // PÁGINA PRINCIPAL DEL DOCENTE
     @GetMapping("/docente")
     public String docenteInicio(Model model, HttpSession session) {
+        
         Usuario usuario = (Usuario) session.getAttribute("usuarioLogueado");
 
         if (usuario == null) {
